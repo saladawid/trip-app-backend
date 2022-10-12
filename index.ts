5a7cd7287070daed957db4b66ecf4d8ff12bf654
saladawid <dawidsalamon@gmail.com>
2022-10-12 15:21:54 +0200

    Added error handling and connection to the base

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError, ValidationError} from './middleware/errors';
 
 const app = express();
 
@@ -8,6 +9,10 @@ app.use(cors({
     origin: 'http://localhost:3000',
 }));
 app.use(express.json());
+
+
+app.use(handleError);
+
 
 app.listen(3001, '0.0.0.0', () => {
     console.log('Listening on port http://localhost:3000');
