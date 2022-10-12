72bfc763a8ad4270c8d2794a77c9494a33c86889
saladawid <dawidsalamon@gmail.com>
2022-10-12 12:18:28 +0200

    Initialize project

---
+++ index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(express.json());
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on port http://localhost:3000');
+});
